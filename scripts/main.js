77d8d633efe6542d7129dc4f6fea7b3ec60fb00a
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2026-02-23 20:40:55 +0700

    that

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 Events.on(EventType.ClientLoadEvent, () => {
     //Screw fog all of my homies HATE fog
-    Events.run(EventType.Trigger.preDraw, () => {Vars.state.rules.fog = false; Vars.state.rules.staticFog = false; Vars.state.rules.limitMapArea = false })
-    })
+    Events.run(EventType.Trigger.preDraw, () => {Vars.state.rules.fog = false; Vars.state.rules.staticFog = false;})
+})
